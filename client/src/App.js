ccc12948b146d82f352f540cde364f2317e46043
Julie Greene <julie_greene22@yahoo.com>
2020-04-13 12:49:11 -0600

    data seeded

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Card, Image, Icon, } from "semantic-ui-react";
+import axios from 'axios';
 
-function App() {
+class App extends React.Component {
+  state = { items: []}
+
+  componentDidMount() {
+    axios.get('/api/items')
+      .then(res => this.setState({ items: res.data, }))
+  }
+
+  // handleSubmit = e => {
+  //   e.preventDefault();
+  //   const { name, image, description, likes } = this.state;
+  //   axios.post('api/items', { name })
+  //     .then( ({ data })) => {
+  //       this.setState({ })
+  //     }
+  // }
+
+
+render(){
+  const { items, } = this.state 
   return (
-    <div className="App">
-      <header> 
-        Working 
-      </header>
-    </div>
-  );
-}
+    <div>
+    <h1>All Items</h1>
+    <hr/> 
+    <Card.Group itemsPerRow={4}>
+    { items.map( item =>
+      <Card key={item.id}>
+        <Image src={item.avatar} />
+        <Card.Content>
+          <Card.Header>
+            { item.name }
+          </Card.Header>
+          <Card.Meta>
+            { item.description }
+          </Card.Meta>
+          <Card.Content extra>
+        <a>
+          <Icon name='user' />
+          {item.likes}
+        </a>
+    </Card.Content>
+        </Card.Content>
+      </Card>
+    )}
+  </Card.Group>
+  </div>
+  )
+    }
+};
+
 
 export default App;
