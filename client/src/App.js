11d020ba62ee30ec92d14566f278672339a3e787
Julie Greene <julie_greene22@yahoo.com>
2020-04-16 23:23:35 -0600

    items rendering to cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,57 +1,101 @@
 import React from 'react';
 import './App.css';
-import { Card, Image, Icon, } from "semantic-ui-react";
+import { Card, Image, Icon, Container, } from "semantic-ui-react";
 import axios from 'axios';
+import styled from 'styled-components';
+import ItemForm from './ItemForm.js';
 
 class App extends React.Component {
   state = { items: []}
 
   componentDidMount() {
     axios.get('/api/items')
-      .then(res => this.setState({ items: res.data, }))
+      .then( (res) => {
+        this.setState({ items: res.data, })
+      })
+      .catch( (err) => {
+        console.log(err)
+      })
   }
-
-  // handleSubmit = e => {
-  //   e.preventDefault();
-  //   const { name, image, description, likes } = this.state;
-  //   axios.post('api/items', { name })
-  //     .then( ({ data })) => {
-  //       this.setState({ })
-  //     }
-  // }
-
-
-render(){
-  const { items, } = this.state 
-  return (
-    <div>
-    <h1>All Items</h1>
-    <hr/> 
-    <Card.Group itemsPerRow={4}>
-    { items.map( item =>
-      <Card key={item.id}>
-        <Image src={item.avatar} />
-        <Card.Content>
-          <Card.Header>
-            { item.name }
-          </Card.Header>
-          <Card.Meta>
-            { item.description }
-          </Card.Meta>
-          <Card.Content extra>
-        <a>
-          <Icon name='user' />
-          {item.likes}
-        </a>
-    </Card.Content>
-        </Card.Content>
-      </Card>
-    )}
-  </Card.Group>
-  </div>
-  )
-    }
+  addItem = (itemName, itemImage, itemDescription, itemLikes) => {
+    axios
+    .post('/api/items', { 
+      name: itemName,
+      image: itemImage,
+      description: itemDescription,
+      likes: itemLikes, 
+    })
+    .then( (res) => {
+      this.setState({
+        items: [
+          { ...res.data, item_id: res.data.id, items: [] },
+          ...this.state.items
+        ]
+      });
+    })
+    .catch(e => {
+      console.log(e);
+    });
 };
 
+render(){
+  const { items, } = this.state
+        return (
+          <AppContainer>
+            <Container>
+   <br/>
+    <h1 style={styles.Header}>Lonely Hearts Club</h1>
+          <br />
+          
+      <Card.Group itemsPerRow={4}>
+        { items.map( item =>
+          <Card key={item.id} style={styles.cardPosition}>
+            <Image src={item.image} />
+            <Card.Content>
+              <Card.Header>
+                { item.name }
+              </Card.Header>
+              <Card.Meta>
+                { item.description}
+              </Card.Meta>
+              <Card.Content extra style={styles.iconPosition}>
+                <Icon name="heart" color="red" /> {item.likes}
+            </Card.Content>
+            </Card.Content>
+          </Card>
+        )}
+      </Card.Group>
+      <ItemForm/>
+      
+      </Container>
+      </AppContainer>
+    )
+  }
+}
+
+const styles = {
+  iconPosition: {
+    float:"right",
+    position: "absolute",
+    bottom: "0",
+    right: "0",
+    clear: "right",
+    paddingRight: "5%"
+  },
+  cardPosition: {
+    marginRight: "10px",
+  },
+  Header: {
+    textAlign: "center",
+    fontSize: "30px",
+    color: "#FFFFFF",
+    fontFamily: "cursive",
+    
+  }
+};
+
+const AppContainer = styled.div`
+  background: linear-gradient(to bottom right, pink, purple);
+`;
 
 export default App;
